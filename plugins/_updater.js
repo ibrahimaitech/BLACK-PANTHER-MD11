bfabe8cd60ced748b81329906d012aa73a65eabb
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-05-02 14:26:49 +0300

    Add files via upload

--- plugins/_updater.js
+++ plugins/_updater.js
@@ -1,107 +1,113 @@
-const DB = require("../lib/scraper");
-const { Config, smd } = require("../lib");
-const simpleGit = require("simple-git");
+const DB = require('../lib/scraper')
+const { Config, smd } = require('../lib')
+const simpleGit = require('simple-git');
 const git = simpleGit();
+const fs = require('fs');
+
+
+if (!fs.existsSync('./.git')) { throw "UPDATE COMMAND NOT WORKS B'COZ GIT NOT FOUND IN APP!" }
 try {
-  const Heroku = require("heroku-client");
+
+  const Heroku = require('heroku-client');
+  //---------------------------------------------------------------------------
+
+
+
+
   async function updateHerokuApp() {
     try {
       const heroku = new Heroku({ token: process.env.HEROKU_API_KEY });
       await git.fetch();
-      const commits = await git.log(["main..origin/main"]);
-      if (commits.total === 0) {
-        return `${Config.botname} IS ON IT'S LATEST VERSION`;
-      } else {
-        console.log("Update Detected, trying to update your bot!");
+      const commits = await git.log(['main..origin/main']);
+      if (commits.total === 0) { return 'You already have latest version installed.'; }
+      else {
+        console.log("Update Detected, trying to update your bot!")
         const app = await heroku.get(`/apps/${process.env.HEROKU_APP_NAME}`);
-        const gitUrl = app.git_url.replace(
-          "https://",
-          `https://api:${process.env.HEROKU_API_KEY}@`
-        );
-        try {
-          await git.addRemote("heroku", gitUrl);
-        } catch (e) {
-          print("Heroku remote adding error", e);
-        }
-        await git.push("heroku", "main");
-        return "Bot updated. Restarting.";
+        const gitUrl = app.git_url.replace('https://', `https://api:${process.env.HEROKU_API_KEY}@`);
+        try { await git.addRemote('heroku', gitUrl); } catch (e) { print('Heroku remote adding error', e); }
+        await git.push('heroku', 'main');
+        return 'Bot updated. Restarting.';
       }
     } catch (e) {
-      print(e);
-      return "Can't Update, Request Denied!";
+      print(e)
+      return "Can't Update, Request Denied!"
     }
   }
-  smd(
-    {
-      pattern: "checkupdate",
-      desc: "Shows repo's refreshed commits.",
-      category: "tools",
-      fromMe: true,
-      react: "🍂",
-      filename: __filename,
-      use: process.env.HEROKU_API_KEY ? "[ start ]" : "",
-    },
+
+
+  //---------------------------------------------------------------------------
+  smd({
+    pattern: "update",
+    desc: "Shows repo\'s refreshed commits.",
+    category: "tools",
+    fromMe: true,
+    react: "🍂",
+    filename: __filename,
+    use: process.env.HEROKU_API_KEY ? "[ start ]" : "",
+  },
     async (citel, text) => {
       try {
-        let commits = await DB.syncgit();
-        if (commits.total === 0)
-          return await citel.reply(
-            `*PANTHER-MD MD IS RUNNING ON LATEST\nPATCHES\nFIXES\UPGRADES*`
-          );
-        let update = await DB.sync();
-        await citel.bot.sendMessage(
-          citel.chat,
-          { text: update.replace(/SuhailTechIMd/, "Astropeda") },
-          { quoted: citel }
-        );
-        if (
-          text == "start" &&
-          process.env.HEROKU_APP_NAME &&
-          process.env.HEROKU_API_KEY
-        ) {
-          citel.reply("Build started...");
+        //console.log("Calle update ")
+        let commits = await DB.syncgit()
+        // console.log("commits:  ", commits)
+        if (commits.total === 0) return await citel.reply(`*BOT IS UPTO DATE...!!*`)
+        let update = await DB.sync()
+        await citel.bot.sendMessage(citel.chat, { text: update.replace(/SuhailTechIMd/, "Astropeda"), }, { quoted: citel });
+
+
+        if (text == 'start' && process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY) {
+          citel.reply('Build started...');
           const update = await updateHerokuApp();
           return await citel.reply(update);
         }
-      } catch (e) {
-        citel.error(`${e}\n\nCommand: update`, e, "ERROR!");
-      }
-    }
-  );
-  smd(
-    {
-      pattern: "update",
-      desc: process.env.HEROKU_API_KEY
-        ? "*UPDATE SUCCESS*"
-        : "UPDATED YOUR DEPLOYEMENT",
-      fromMe: true,
-      category: "tools",
-      filename: __filename,
-    },
+
+
+
+      } catch (e) { citel.error(`${e}\n\nCommand: update`, e, "ERROR!") }
+
+
+
+    })
+
+
+
+
+  //---------------------------------------------------------------------------
+  //                  UPDATE COMMANDS
+  //---------------------------------------------------------------------------
+
+
+  smd({
+    pattern: "updatenow",
+    desc: process.env.HEROKU_API_KEY ? "Temporary update for heroku app!" : "update your bot by repo!.",
+    fromMe: true,
+    category: "tools",
+    filename: __filename
+  },
     async (citel) => {
       try {
-        let commits = await DB.syncgit();
-        if (commits.total === 0)
-          return await citel.reply(`*${Config.VERSION} IS Updating*`);
-        let update = await DB.sync();
-        let text = ` 
-*UPDATE RUNNING*
-\t${update}*`;
+        let commits = await DB.syncgit()
+        if (commits.total === 0) return await citel.reply(`*YOU HAVE LATEST VERSION INSTALLED!*`)
+        let update = await DB.sync()
+        let text = " *> Please Wait Updater Started...!*\n  *───────────────────────────*\n" + update + "\n  *───────────────────────────*";
         await citel.bot.sendMessage(citel.jid, { text });
-        await require("simple-git")().reset("hard", ["HEAD"]);
-        await require("simple-git")().pull();
-        await citel.reply(
-          process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY
-            ? "*`BOT UPDATED`*\n*RESTART YOUR BOT FOR UPDATE TO TAKE EFFECT*"
-            : "```*Successfully updated. Now You Have Latest Version Installed!*"
-        );
-      } catch (e) {
-        citel.error(`${e}\n\nCommand: updatenow`, e, "ERROR!");
-      }
-    }
-  );
+        await require("simple-git")().reset("hard", ["HEAD"])
+        await require("simple-git")().pull()
+        await citel.reply(process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY ? "*BOT Temporary Updated on `HEROKU`!\nIt'll reset when your bot restarts!*" : "*Successfully updated. Now You Have Latest Version Installed!*")
+        // process.exit(1);
+
+      } catch (e) { citel.error(`${e}\n\nCommand: updatenow`, e, "ERROR!") }
+    })
+
+
+
+
+
+
+
   if (process.env.HEROKU_API_KEY) {
     print("HEROKU : checking for auto update!");
-    updateHerokuApp();
+    updateHerokuApp()
   }
-} catch (e) {}
+
+} catch (e) { }
