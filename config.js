95ae4eea761c557851020b4d9e53bbf9b9993261
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-05-02 14:23:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -78,8 +78,8 @@ global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]
 
 
 //========================= [ BOT SETTINGS ] =========================\\
-global.style = process.env.STYLE   || '2',  // put '1' to "5" here to check bot styles
-global.flush = process.env.FLUSH   || "true"; // Make it "true" if bot not responed
+global.style = process.env.STYLE   || '1',  // put '1' to "5" here to check bot styles
+global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
 global.gdbye = process.env.GOODBYE || "false"; 
 global.wlcm  = process.env.WELCOME || "false";  // Make it "false" for disable WELCOME 
 
@@ -111,7 +111,7 @@ global.SESSION_ID = process.env.SESSION_ID ||  "" ; // PUT your SESSION_ID
 
 module.exports = {
 
-  menu: process.env.MENU || "Aztec_Md", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Gifted-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
+  menu: process.env.MENU || "suhail_Md", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Panther-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
 
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
